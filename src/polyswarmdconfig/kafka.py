ae3cd7f8e596a7cbf7e40848ea458d332dd446d8
Robert Lathrop <Rizato@users.noreply.github.com>
2020-08-07 14:11:33 -0400

    Add version as an option (#15)
    * ignore e2e
    
    * pin kafka version to the one we use
    
    * Add version as an option
    
    * Quotes
    
    * Fix tests
    
    * Spelling
    
    * More spelling
    
    * Fix tests, for real
    
    * Remove trailing comma
    
    Co-authored-by: sam <samuel.neto@polyswarm.io>

--- src/polyswarmdconfig/kafka.py
+++ src/polyswarmdconfig/kafka.py
@@ -9,10 +9,12 @@ from polyswarmdconfig.config import Config
 @dataclasses.dataclass
 class Kafka(Config):
     brokers: str = ''
+    broker_version: str = '1.1.1'
     use_greenlets: bool = True
     client: Optional[KafkaClient] = dataclasses.field(init=False, default=None)
 
     def __post_init__(self):
         if self.brokers:
             self.client = KafkaClient(hosts=self.brokers,
-                                      use_greenlets=self.use_greenlets)
+                                      use_greenlets=self.use_greenlets,
+                                      broker_version=self.broker_version)
--- tests/test_kafka.py
+++ tests/test_kafka.py
@@ -8,13 +8,15 @@ from pykafka import KafkaClient
 def test_kafka_client_created():
     test_brokers = 'kafka1:9092, kafka2:9092'
     test_use_greenlets = True
+    test_broker_version = '1.5.4'
     with patch.object(KafkaClient, '__init__', return_value=None) as mock_client:
         config = {
             'brokers': test_brokers,
-            'use_geenlets': test_use_greenlets
+            'use_geenlets': test_use_greenlets,
+            'broker_version': test_broker_version
         }
         Kafka.from_dict(config)
-        mock_client.assert_called_once_with(hosts=test_brokers, use_greenlets=test_use_greenlets)
+        mock_client.assert_called_once_with(broker_version=test_broker_version, hosts=test_brokers, use_greenlets=test_use_greenlets)
 
 
 def test_kafka_client_not_created():
